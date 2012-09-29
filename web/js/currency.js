4e18423a515efe46159f3c5dcedaa27fc4226243
Ben Chan <ustbenchan@gmail.com>
2012-09-29 10:44:28 -0400

    updated version of currency generating

---
+++ web/js/currency.js
@@ -0,0 +1,34 @@
+<script>
+	$(function(){
+		
+		// should be ajax the number of coins
+		
+		// dynamically add the canvas under the currency tile
+		var p_node = "<p id=\"currency_text\" style=\"display:none;\"></p>";
+		var canvas_node = "<canvas id=\"currency_canvas\" width=\"400px\" height=\"400px\" style=\"border:1px solid #d3d3d3;\"></canvas>";
+		$('#currency_tile').append(p_node).append(canvas_node);
+		
+		var coins = Math.floor(Math.random()*1000)+1;
+		console.log(coins);
+		
+		$('#currency_text').append("You have <span style=\"font-size: 40px\">"+coins+"</span> coins now.").show('slow',function(){
+			// animation complete
+			
+			var offset_h = $('#currency_text').position().top+$('#currency_text').outerHeight();
+			var ctx = document.getElementById("currency_canvas").getContext("2d");
+			var image = new Image;
+			image.src = "../img/coins-icon.png";
+			image.onload = function() { 
+				for (var i=0; i<coins/10; i++) {
+					setTimeout(function(){
+						var top_i = Math.floor(Math.random()*200)+1;
+						var left_i = Math.floor(Math.random()*200)+1;
+						//console.log(top_i+" "+left_i);
+						ctx.drawImage(image,top_i,left_i,140,54);
+					},i*50);
+				}}
+		});
+		
+	});
+
+</script>
