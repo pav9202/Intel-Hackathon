eb9532e0037db9b55c84a5cb526a275b006b4535
Pavleen Thukral <pav920@gmail.com>
2012-09-29 04:30:40 -0400

    Added day over today
    bargraph

--- web/js/plot1.js
+++ web/js/plot1.js
@@ -61,7 +61,6 @@ var stepsThisWeek = function() {
 	 req.open("GET", url, true);
 	 req.send();
 	 */
-<<<<<<< HEAD
 	var step = '1,2,3,2,5,6,15,8,8';
 	var timestamp = "2012-09-21 14:04:59:612,2012-09-22 14:04:59:612,2012-09-23 14:04:59:612,2012-09-24 14:04:59:612,2012-09-25 14:04:59:612,2012-09-26 14:04:59:612,2012-09-28 14:04:59:612,2012-09-28 14:04:59:612,2012-09-29 14:04:59:612";
 	var s1 = step.split(',');
@@ -71,36 +70,31 @@ var stepsThisWeek = function() {
 	var lastone = "";
 	var maxWeight = 0;
 	var d = new Date();
-	for(i = 0;i<t1.length;i++){
+	for ( i = 0; i < t1.length; i++) {
 		var t2 = t1[i].split(" ");
 		var t3 = t2[0].split("-");
-		
-		if(t3[2]!= lastone && parseInt(t3[2],10)>parseInt(d.toDateString().split(" ")[2],10)-7){
-		//alert(t3[2]);
-			if(parseInt(lastone,10) != parseInt(t3[2],10)-1){
-				for(z=parseInt(lastone,10)+1;z<parseInt(t3[2],10);z++){
+
+		if (t3[2] != lastone && parseInt(t3[2], 10) > parseInt(d.toDateString().split(" ")[2], 10) - 7) {
+			//alert(t3[2]);
+			if (parseInt(lastone, 10) != parseInt(t3[2], 10) - 1) {
+				for ( z = parseInt(lastone, 10) + 1; z < parseInt(t3[2], 10); z++) {
 					steps.push(0);
 					timestamps.push('');
 				}
 			}
 			lastone = t3[2];
 			steps.push(s1[i]);
-			if(parseInt(s1[i],10)>maxWeight){
-				maxWeight = parseInt(s1[i],10);
+			if (parseInt(s1[i], 10) > maxWeight) {
+				maxWeight = parseInt(s1[i], 10);
 			}
 			timestamps.push(t3[2]);
 		}
 	}
 
-	
-	
 	alert("HELLOOOOOOOOOOOO");
 	
-	$.jqplot('chart2', [steps], {
-=======
-	/*alert("HELLOOOOOOOOOOOO");*/
-	$.jqplot('weightLoss', [[3, 7, 9, 1, 4, 6, 8, 2, 5]], {
->>>>>>> iframes and tiles
+	var options ={
+
 		// Give the plot a title.
 		title : 'Steps Per Day This Past Week',
 		axesDefaults : {
@@ -111,35 +105,35 @@ var stepsThisWeek = function() {
 		// Up to 9 y axes are supported.
 		axes : {
 			// options for each axis are specified in seperate option objects.
-			
+
 			xaxis : {
-				show: false,
-				tickOptions: {
-					showGridline: false,
-					showMark: false,
-					showLabel: false
+				show : false,
+				tickOptions : {
+					showGridline : false,
+					showMark : false,
+					showLabel : false
 				}
 			},
 			yaxis : {
-				tickOptions: {
-					angle:270,
-					formatString: '%d'
+				tickOptions : {
+					angle : 270,
+					formatString : '%d'
 				},
-				min: 0,
-				max: maxWeight+2
+				min : 0,
+				max : maxWeight + 2
 			}
 		},
 		series : [{
-			pointLabels: {
-				show: true
+			pointLabels : {
+				show : true
 			},
-			trendline: {
-				show: true,
-//				renderer: $.jqplot.LineRenderer(),
-				color: '#7FFF00',
-				lable: '',
-				type: 'exp',
-				lineWidth: 1.5
+			trendline : {
+				show : true,
+				//				renderer: $.jqplot.LineRenderer(),
+				color : '#7FFF00',
+				lable : '',
+				type : 'exp',
+				lineWidth : 1.5
 			},
 			markerOptions : {
 				style : "circle",
@@ -147,7 +141,20 @@ var stepsThisWeek = function() {
 			}
 
 		}]
-		
+
+	};
+	var plot1= $.jqplot('chart2', [steps], options);
+	
+	$("#chart2").resizable({
+		delay : 20
+	});
+
+	$('#chart2').bind('resize', function(event, ui) {
+		// pass in resetAxes: true option to get rid of old ticks and axis properties
+		// which should be recomputed based on new plot size.
+		plot1.replot({
+			resetAxes : true
+		});
 	});
 
 };
