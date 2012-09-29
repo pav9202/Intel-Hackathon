60835ddb1c2a345e4f1d6fe09d013f65fe1b6df4
Pavleen Thukral <pav920@gmail.com>
2012-09-28 20:53:21 -0400

    trying to add a bubble plot... not really working yet but I want to pull

---
+++ web/js/activityperiods.js
@@ -0,0 +1,19 @@
+var periodBubbles = new function(){
+	var arr = [[11, 123, 1236, "Acura"], [45, 92, 1067, "Alfa Romeo"], 
+    [24, 104, 1176, "AM General"], [50, 23, 610, "Aston Martin Lagonda"], 
+    [18, 17, 539, "Audi"], [7, 89, 864, "BMW"], [2, 13, 1026, "Bugatti"]];
+    alert('YOYOYO');
+    $.jqplot('chartdiv',[arr],{
+        title: 'Bubble Chart with Gradient Fills',
+        seriesDefaults:{
+            renderer: $.jqplot.BubbleRenderer,
+            rendererOptions: {
+                bubbleAlpha: 0.6,
+                highlightAlpha: 0.8
+            },
+            shadow: true,
+            shadowAlpha: 0.05
+        }
+    });
+	
+};
